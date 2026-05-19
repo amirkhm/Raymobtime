1e4ba3991be31f0d5c62e541b112f0263e3a1512
Gabriel <gabriel.vieira@itec.ufpa.br>
2026-05-19 16:49:19 -0300

    minimalist config.py

--- src/scripts/config.py
+++ src/scripts/config.py
@@ -47,6 +47,12 @@ def load_config():
 
 
 def get_lat_long(base_insite_project_path):
+    """
+    Extract latitude and longitude from the base.txrx file of a Wireless InSite project.
+    Args:       base_insite_project_path (str): Path to the base InSite project directory.
+    Returns:    tuple[str, str]: A tuple containing (latitude, longitude) as strings.
+    Raises:     FileNotFoundError: If the base.txrx file does not exist.
+    """
     txrx_file = open(os.path.join(base_insite_project_path, 'base.txrx'), 'r')
     latitude = False
     longitude = False
@@ -61,14 +67,20 @@ def get_lat_long(base_insite_project_path):
 
 
 def get_insite_version(base_insite_project_path):
+    """
+    Parse and return the Wireless InSite version from the model.study.xml file.
+    Args:       base_insite_project_path (str): Path to the base InSite project directory.
+    Returns:    str: The InSite version (e.g., '3.0', '3.2').
+    Raises:     FileNotFoundError: If the model.study.xml file does not exist.
+    """
     model_file = open(os.path.join(base_insite_project_path, 'model.study.xml'), 'r')
     insite_version = False
-
     for line in model_file:
         if '<InSite version="' in line:
             insite_version = line.split('version=')[1].split(' ')[0][1:4]
+            model_file.close()
             return insite_version
-        
+
 def base_run_dir_fn(i):
     """
     Returns the run_dir for run i.
@@ -81,110 +93,50 @@ def base_run_dir_fn(i):
     return "run{:05d}".format(i)
 
 
-###############################################################
-## Most information in this configuration file is used in the Stage 1 of
-## the three stages below. But some are also used in the other stages.
-## Stage 1: Running the ray-tracing (RT) and traffic simulators
-## Stage 2: Organizing raw data into a 5GMdata database
-## Stage 3: Converting the database into a file suitable to machine learning packages such as Keras
-## This file is split into two parts. In most cases Part II is not modified.
-###############################################################
+cfg = load_config() # load yaml
 
-# Read typical information from config.yaml
-cfg = load_config()
-
-###############################################################
-## Part I - Basic information that typically needs to be modified / checked
-###############################################################
-
-# Current folder (or directory). Some paths are relative to this folder:
 working_directory = os.path.dirname(os.path.realpath(__file__))
-
-# InSite will look for input files in this folder. These files will be used to generate all simulations
 base_insite_project_path = os.path.join(
     working_directory,
     'base_files',
-    cfg.simulation_paths.base_insite_path
-)
-
-# Folder to store each InSite project and its results
-# Will create subfolders for each "run", run0000, run0001, etc.
+    cfg.simulation_paths.base_insite_path)
 results_dir = os.path.join(
     working_directory,
     'simulations',
-    cfg.simulation_paths.results_dir_path
-)
-
+    cfg.simulation_paths.results_dir_path)
 isolated_results_dir = os.path.join(
     results_dir,
-    cfg.simulation_paths.base_insite_path
-)
-
-# Folders and files for InSite and its license.
-# For Windows you may simply inform the path to the executable files,
-# not minding about the license file location.
-# Folders for SUMO and InSite.
-# Use executable sumo-gui if want to see the GUI or sumo otherwise.
-
+    cfg.simulation_paths.base_insite_path)
 sim_name = cfg.simulation_paths.results_dir_path
 isolated_sim = cfg.features.isolated_simulation
-
-# Identify InSite version
 insite_version = get_insite_version(base_insite_project_path)
-
 # InSite env variable
 locale = 'LC_CTYPE=en_US.UTF-8 LC_NUMERIC=en_US.UTF-8 LC_TIME=en_US.UTF-8 '
+
 
 if insite_version == '3.3':
     wibatch_bin = (
         locale
-        + '{} '.format(cfg.insite_paths.REMCOMINC_LICENSE_FILE)
-        + 'LD_LIBRARY_PATH={}/OpenMPI/1.4.4/Linux-x86_64RHEL6/lib/ '.format(
-            cfg.insite_paths.insite_software_path
-        )
-        + '{}/WirelessInSite/3.3.0.4/Linux-x86_64RHEL6/bin/wibatch'.format(
-            cfg.insite_paths.insite_software_path
-        )
-    )
-
+        + f"{cfg.insite_paths.REMCOMINC_LICENSE_FILE} "
+        + f"LD_LIBRARY_PATH={cfg.insite_paths.insite_software_path}/OpenMPI/1.4.4/Linux-x86_64RHEL6/lib/ "
+        + f"{cfg.insite_paths.insite_software_path}/WirelessInSite/3.3.0.4/Linux-x86_64RHEL6/bin/wibatch")
 elif insite_version == '3.2':
     wibatch_bin = (
         locale
-        + '{} '.format(cfg.insite_paths.REMCOMINC_LICENSE_FILE)
-        + 'LD_LIBRARY_PATH=/{}/OpenMPI/1.4.4/Linux-x86_64RHEL6/lib/ '.format(
-            cfg.insite_paths.insite_software_path
-        )
-        + '{}/WirelessInSite/3.2.0.3/Linux-x86_64RHEL6/bin/wibatch'.format(
-            cfg.insite_paths.insite_software_path
-        )
-    )
+        + f"{cfg.insite_paths.REMCOMINC_LICENSE_FILE} "
+        + f"LD_LIBRARY_PATH={cfg.insite_paths.insite_software_path}/OpenMPI/1.4.4/Linux-x86_64RHEL6/lib/ "
+        + f"{cfg.insite_paths.insite_software_path}/WirelessInSite/3.2.0.3/Linux-x86_64RHEL6/bin/wibatch")
+
+use_fixed_receivers = cfg.features.use_fixed_receivers
+use_pedestrians = cfg.features.use_pedestrians
+use_vehicles_template = cfg.features.use_vehicles_template
+drone_simulation = cfg.features.drone_simulation
+mimo_orientation = cfg.features.mimo_orientation
+use_V2V = cfg.features.use_V2V
 
 n_run = range(1)
 
-# Set to False if only vehicles are receivers
-use_fixed_receivers = cfg.features.use_fixed_receivers
-
-# Only set True if your SUMO is ready for pedestrians
-use_pedestrians = cfg.features.use_pedestrians
-
-# Set True to use pre-made vehicle, not boxes.
-# NOTE: only set True if you have the folder objects with the models.
-use_vehicles_template = cfg.features.use_vehicles_template
-
-# Only drones will be chosen to be receivers
-drone_simulation = cfg.features.drone_simulation
-
-# Only available for a single Rx
-mimo_orientation = cfg.features.mimo_orientation
-
-# Set True to use V2V, where transmitters and receivers are vehicles
-use_V2V = cfg.features.use_V2V
-
-
 if not isolated_sim:
-    ### HERE STARTS CONFIGURATION ###
-    ### NOTE: ONLY CHANGE IF YOU KNOW WHAT YOU ARE DOING
-
     # SUMO configuration file
     sumo_bin = cfg.sumo_files.sumo_bin
     sumo_cfg = os.path.join(
@@ -203,61 +155,22 @@ if not isolated_sim:
         '{}.sumo.cfg'.format(cfg.sumo_files.sumo_cfg)
     )
 
-    # print('########## Scripts will assume the following files: ##########')
-    # print('SUMO executable: ', sumo_bin)
-    # print('SUMO configuration: ', sumo_cfg)
-    # print('InSite calcprop executable: ', calcprop_bin)
-    # print('InSite wibatch executable: ', wibatch_bin)
-    # print('Working folder (base for several folders): ', working_directory)
-    # print('InSite input files folder: ', base_insite_project_path)
-    # print('Final output parent folder: ', results_dir)
-
     # Iterator that determines maximum number of RT simulations
     n_run = range(
         cfg.simulation_parameters.n_init_run,
-        cfg.simulation_parameters.n_end_run,
-        1
-    )
-
-    # Time interval between scenes, in seconds
+        cfg.simulation_parameters.n_end_run)
     sampling_interval = float(cfg.simulation_parameters.sampling_interval)
-
-    # Number of scenes of each episode
     time_of_episode = int(cfg.simulation_parameters.n_scenes_of_each_episode)
+    time_between_episodes = int(float(cfg.simulation_parameters.time_between_episodes) / sampling_interval)
 
-    # Time among episodes, in steps
-    # If you specify x/Ts, then x is in seconds
-    time_between_episodes = int(
-        float(cfg.simulation_parameters.time_between_episodes) / sampling_interval
-    )
-
-
-if use_fixed_receivers:
-    # Number of receivers per episode
-    n_antenna_per_episode = 0
-else:
-    # Number of receivers per episode
-    n_antenna_per_episode = cfg.simulation_parameters.n_antenna_per_episode
-
+n_antenna_per_episode = 0 if use_fixed_receivers else cfg.simulation_parameters.n_antenna_per_episode
 
 if use_V2V:
-    # Number of transmitters per episode
     n_Tx_per_episode = cfg.simulation_parameters.n_Tx_per_episode
-
-    # Number of receivers per episode
     n_antenna_per_episode = cfg.simulation_parameters.n_antenna_per_episode
 
-
-# Where to map the receiver to TFRecords: minx, miny, maxx, maxy
 analysis_area_enabled = cfg.data_handler.area_of_analyses.enabled
 analysis_area = cfg.data_handler.area_of_analyses.coordinates_limits
-analysis_area_resolution = 0.5
-
-antenna_number = 10
-
-# Frequency in Hz for the RT simulation
-frequency = 60e9
-
 set_area_limit = cfg.area_limits.enabled
 max_lim = cfg.area_limits.max_lim
 min_lim = cfg.area_limits.min_lim
@@ -274,12 +187,10 @@ Tx_position = cfg.data_handler.cartesian_lidar_matrix.Tx_position
 max_dist_LIDAR = cfg.data_handler.cartesian_lidar_matrix.max_dist_LIDAR
 type_data = cfg.data_handler.cartesian_lidar_matrix.type_data
 
-
 if use_V2V:
     close_vehicles = cfg.v2v_options.close_vehicles
     n_of_vehicles = cfg.v2v_options.n_of_vehicles
     chosen_vehicle = cfg.v2v_options.chosen_vehicle
-
 
 import_precoding = cfg.data_handler.antenna_arr.import_precoding
 import_hmatrix = cfg.data_handler.antenna_arr.import_hmatrix
@@ -291,67 +202,31 @@ normalized_antenna_distance = cfg.data_handler.antenna_arr.normalized_antenna_di
 blensor_scenario_path = cfg.blensor_options.path_to_scenario
 blensor_runfile_path = cfg.blensor_options.blensor_img_path
 
-
-###############################################################
-## Part II - Extra information that typically does not need to be modified
-## unless you changed the InSite model, using the GUI, for example.
-###############################################################
-
-# Fullfill this parameters with InSite's information
 insite_study_area_name = cfg.base_files_names.insite_study_area_name
 insite_tx_name = cfg.base_files_names.insite_tx_name
 insite_rx_name = cfg.base_files_names.insite_rx_name
 insite_setup_name = cfg.base_files_names.insite_setup_name
 insite_vehicles_name = cfg.base_files_names.insite_vehicles_name
 
-
 if use_vehicles_template:
     latitude, longitude = get_lat_long(base_insite_project_path)
     insite_vehicles_name_model = insite_vehicles_name
     insite_vehicles_name = insite_vehicles_name + '_'
 
-
-##### Folders and files for InSite ####
-
-# Copy of the RWI project used in the simulation
-# AK-TODO: instead of "base", it should match the name InSite gives,
-# to facilitate porting.
 if isolated_sim:
     results_base_model_dir = isolated_results_dir
 else:
     results_base_model_dir = os.path.join(results_dir, 'base')
 
 results_base_model_dir.replace('\\', '/')
-
-# Input files, which are read by the Python scripts
-
-# File that has the base InSite project
 setup_path = os.path.join(base_insite_project_path, insite_setup_name + '.setup')
 base_setup_path = os.path.join(base_insite_project_path, 'base.setup')
-
-# setup_path = setup_path.replace(' ', '\ ') # deal with paths with blank spaces
-
-# XML that has information about the simulations
 base_x3d_xml_path = os.path.join(
     base_insite_project_path,
-    'base.' + insite_study_area_name + '.xml'
-)
-
-# Name, basename, of the paths file generated in the simulation
+    'base.' + insite_study_area_name + '.xml')
 paths_file_name = insite_setup_name + '.paths.t001_01.r002.p2m'
-
-# Base object file to generate the object_dst_file_name
 base_object_file_name = os.path.join(base_insite_project_path, "base.object")
-
-# Base txrx file to generate the txrx_dst_file_name
 base_txrx_file_name = os.path.join(base_insite_project_path, "base.txrx")
-
-
-# Output files, which are written by the Python scripts.
-# Provide here only the names.
-# The full paths will be created by simulation.py, using the run folder.
-
-# Name, basename, of the JSON output simulation info file
 simulation_info_file_name = 'wri-simulation.info'
 
 # Object which will be modified in the RWI project
